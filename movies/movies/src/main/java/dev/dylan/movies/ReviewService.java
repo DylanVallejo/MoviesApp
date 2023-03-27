44ebdaa5b480ca890a67e973ff0bba000b3c3626
DylanVallejo <dylan1811vallejo@gmail.com>
2023-03-26 20:59:23 -0500

    creating the fornt side for the movies app also creating multiple components to show one movie a carousel of movies buton to navigate whit react router dom form to create a reviews on a single movie also a media player to play the movie trailer using bootsrap and material ui adidng in a lonely file the axios config

--- movies/movies/src/main/java/dev/dylan/movies/ReviewService.java
+++ movies/movies/src/main/java/dev/dylan/movies/ReviewService.java
@@ -16,7 +16,7 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
+        Review review =  reviewRepository.insert( new Review(reviewBody));
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
